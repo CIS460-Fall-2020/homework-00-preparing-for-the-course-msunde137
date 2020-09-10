914374326e16de5715073a51d929f5d548e2e590
CosmicPotato <54864515+msunde137@users.noreply.github.com>
2020-09-09 22:25:26 -0400

    1st commit

---
+++ build-460-Desktop-Debug/debug/moc_predefs.h
@@ -0,0 +1,8 @@
+# 1 "..\\..\\..\\..\\Users\\imacs\\anaconda3\\Library\\mkspecs\\features\\data\\dummy.cpp"
+# 1 "<built-in>" 1
+# 1 "<built-in>" 3
+# 366 "<built-in>" 3
+# 1 "<command line>" 1
+# 1 "<built-in>" 2
+# 1 "..\\..\\..\\..\\Users\\imacs\\anaconda3\\Library\\mkspecs\\features\\data\\dummy.cpp" 2
+
